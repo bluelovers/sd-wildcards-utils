22e036e65ff65b2e569389da2e988a6b99ada1c2
bluelovers <codelovers@users.sourceforge.net>
2024-05-23 00:36:13 +0800

    build: update build

--- dist/index.cjs.development.cjs
+++ dist/index.cjs.development.cjs
@@ -4,6 +4,127 @@ Object.defineProperty(exports, '__esModule', { value: true });
 
 var yaml = require('yaml');
 
+const RE_DYNAMIC_PROMPTS_WILDCARDS = /__([&~!@])?([*\w\/_\-]+)(\([^\n#]+\))?__/;
+/**
+ * Checks if the input string matches the dynamic prompts wildcards pattern.
+ *
+ * @param input - The input string to check.
+ * @returns A boolean indicating whether the input string matches the pattern.
+ *
+ * @remarks
+ * This function uses the `matchDynamicPromptsWildcards` function to perform the check.
+ * It returns `true` if the input string is a full match, and `false` otherwise.
+ *
+ * @example
+ * ```typescript
+ * const input1 = "__season_clothes(season=winter)__";
+ * console.log(isDynamicPromptsWildcards(input1)); // Output: true
+ *
+ * const input2 = "__season_clothes(season=__season_clothes__)__";
+ * console.log(isDynamicPromptsWildcards(input2)); // Output: true
+ *
+ * const input3 = "This is not a wildcards pattern";
+ * console.log(isDynamicPromptsWildcards(input3)); // Output: false
+ * ```
+ */
+function isDynamicPromptsWildcards(input) {
+  return matchDynamicPromptsWildcards(input).isFullMatch;
+}
+/**
+ * Matches the input string against the dynamic prompts wildcards pattern.
+ *
+ * @see https://github.com/adieyal/sd-dynamic-prompts/blob/main/docs/SYNTAX.md
+ *
+ * @param input - The input string to match.
+ * @returns An object containing the matched groups or `null` if no match is found.
+ *
+ * @remarks
+ * This function uses the `RE_DYNAMIC_PROMPTS_WILDCARDS` regular expression to perform the match.
+ * The returned object contains the following properties:
+ * - `name`: The name extracted from the input string.
+ * - `variables`: The variables extracted from the input string.
+ * - `keyword`: The keyword extracted from the input string.
+ * - `source`: The original matched source string.
+ * - `isFullMatch`: A boolean indicating whether the input string is a full match.
+ *
+ * @example
+ * ```typescript
+ * const input = "\_\_season_clothes(season=winter)\_\_";
+ * const result = matchDynamicPromptsWildcards(input);
+ * console.log(result);
+ * // Output: { name: 'season_clothes', variables: '(season=winter)', keyword: undefined, source: '\__season_clothes(season=winter)\__', isFullMatch: true }
+ * ```
+ *
+ * @example
+ * __season_clothes(season=winter)__
+ * __season_clothes(season=__season_clothes__)__
+ * __season_clothes(season=!__season_clothes__)__
+ *
+ * __season_clothes(season=__@season_clothes__)__
+ * __season_clothes(season=__~season_clothes__)__
+ *
+ * __@season_clothes(season=__season_clothes__)__
+ * __~season_clothes(season=__season_clothes__)__
+ *
+ * __season_clothes(season={summer|autumn|winter|spring})__
+ * __season_clothes(season=!{summer|autumn|winter|spring})__
+ *
+ * __season_clothes(season={@summer|autumn|winter|spring})__
+ * __season_clothes(season={!summer|autumn|winter|spring})__
+ *
+ * __season_clothes(season=)__
+ */
+function matchDynamicPromptsWildcards(input) {
+  let m = input.match(RE_DYNAMIC_PROMPTS_WILDCARDS);
+  if (!m) return null;
+  let [source, keyword, name, variables] = m;
+  return {
+    name,
+    variables,
+    keyword,
+    source,
+    isFullMatch: source === input
+  };
+}
+/**
+ * Checks if the given name is a valid Wildcards name.
+ *
+ * @param name - The name to check.
+ * @returns A boolean indicating whether the name is valid.
+ *
+ * @remarks
+ * A valid Wildcards name should:
+ * - Only contain alphanumeric characters, hyphens, or underscores.
+ * - Not start or end with an underscore.
+ * - Not contain consecutive underscores.
+ *
+ * @example
+ * ```typescript
+ * const name1 = "season_clothes";
+ * console.log(isWildcardsName(name1)); // Output: true
+ *
+ * const name2 = "_season_clothes";
+ * console.log(isWildcardsName(name2)); // Output: false
+ *
+ * const name3 = "season_clothes_";
+ * console.log(isWildcardsName(name3)); // Output: false
+ *
+ * const name4 = "season__clothes";
+ * console.log(isWildcardsName(name4)); // Output: false
+ *
+ * const name5 = "season-clothes";
+ * console.log(isWildcardsName(name5)); // Output: true
+ * ```
+ */
+function isWildcardsName(name) {
+  return /^[\w\-_]+$/.test(name) && !/__|_$|^_/.test(name);
+}
+function assertWildcardsName(name) {
+  if (isWildcardsName(name)) {
+    throw new SyntaxError(`Invalid Wildcards Name Syntax: ${name}`);
+  }
+}
+
 function _validMap(key, node) {
   const elem = node.items.find(pair => pair.value === null);
   if (elem) {
@@ -25,26 +146,30 @@ function validWildcardsYamlData(data, opts) {
   }
 }
 const RE_UNSAFE_QUOTE = /['"]/;
-const RE_UNSAFE_VALUE = /^\s*-|[{$~!@}\n|:?]/;
+const RE_UNSAFE_VALUE = /^\s*-|[{$~!@}\n|:?#]/;
 function normalizeDocument(doc) {
   yaml.visit(doc, {
     Map: _validMap,
     // @ts-ignore
     Scalar(key, node) {
       let value = node.value;
-      if (RE_UNSAFE_QUOTE.test(value)) {
-        throw new SyntaxError(`Invalid SYNTAX. ${key} => ${node}`);
-      } else if (node.type === 'QUOTE_DOUBLE' || node.type === 'QUOTE_SINGLE' && !value.includes('\\')) {
-        node.type = 'PLAIN';
-      }
-      value = value.replace(/[\x00\u200b]+/g, '').replace(/[\s\xa0]+|\s+$/gm, ' ');
-      if (RE_UNSAFE_VALUE.test(value)) {
-        if (node.type === 'PLAIN') {
-          node.type = 'BLOCK_LITERAL';
+      if (typeof value === 'string') {
+        if (RE_UNSAFE_QUOTE.test(value)) {
+          throw new SyntaxError(`Invalid SYNTAX. ${key} => ${node}`);
+        } else if (node.type === 'QUOTE_DOUBLE' || node.type === 'QUOTE_SINGLE' && !value.includes('\\')) {
+          node.type = 'PLAIN';
         }
-        value = value.replace(/^\s+|\s+$/g, '').replace(/\n\s*\n/g, '\n');
+        value = value.replace(/[\x00\u200b]+/g, '').replace(/[\s\xa0]+|\s+$/gm, ' ');
+        if (RE_UNSAFE_VALUE.test(value)) {
+          if (node.type === 'PLAIN') {
+            node.type = 'BLOCK_LITERAL';
+          } else if (node.type === 'BLOCK_FOLDED' && /#/.test(value)) {
+            node.type = 'BLOCK_LITERAL';
+          }
+          value = value.replace(/^\s+|\s+$/g, '').replace(/\n\s*\n/g, '\n');
+        }
+        node.value = value;
       }
-      node.value = value;
     }
   });
 }
@@ -118,8 +243,13 @@ function parseWildcardsYaml(source, opts) {
   return data;
 }
 
+exports.RE_DYNAMIC_PROMPTS_WILDCARDS = RE_DYNAMIC_PROMPTS_WILDCARDS;
 exports._validMap = _validMap;
+exports.assertWildcardsName = assertWildcardsName;
 exports.default = parseWildcardsYaml;
+exports.isDynamicPromptsWildcards = isDynamicPromptsWildcards;
+exports.isWildcardsName = isWildcardsName;
+exports.matchDynamicPromptsWildcards = matchDynamicPromptsWildcards;
 exports.normalizeDocument = normalizeDocument;
 exports.parseWildcardsYaml = parseWildcardsYaml;
 exports.stringifyWildcardsYamlData = stringifyWildcardsYamlData;
--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,5 +1,112 @@
 import { CreateNodeOptions, Document as Document$1, DocumentOptions, Node as Node$1, ParseOptions, ParsedNode, SchemaOptions, ToStringOptions, YAMLMap } from 'yaml';
 
+export declare const RE_DYNAMIC_PROMPTS_WILDCARDS: RegExp;
+/**
+ * Checks if the input string matches the dynamic prompts wildcards pattern.
+ *
+ * @param input - The input string to check.
+ * @returns A boolean indicating whether the input string matches the pattern.
+ *
+ * @remarks
+ * This function uses the `matchDynamicPromptsWildcards` function to perform the check.
+ * It returns `true` if the input string is a full match, and `false` otherwise.
+ *
+ * @example
+ * ```typescript
+ * const input1 = "__season_clothes(season=winter)__";
+ * console.log(isDynamicPromptsWildcards(input1)); // Output: true
+ *
+ * const input2 = "__season_clothes(season=__season_clothes__)__";
+ * console.log(isDynamicPromptsWildcards(input2)); // Output: true
+ *
+ * const input3 = "This is not a wildcards pattern";
+ * console.log(isDynamicPromptsWildcards(input3)); // Output: false
+ * ```
+ */
+export declare function isDynamicPromptsWildcards(input: string): boolean;
+/**
+ * Matches the input string against the dynamic prompts wildcards pattern.
+ *
+ * @see https://github.com/adieyal/sd-dynamic-prompts/blob/main/docs/SYNTAX.md
+ *
+ * @param input - The input string to match.
+ * @returns An object containing the matched groups or `null` if no match is found.
+ *
+ * @remarks
+ * This function uses the `RE_DYNAMIC_PROMPTS_WILDCARDS` regular expression to perform the match.
+ * The returned object contains the following properties:
+ * - `name`: The name extracted from the input string.
+ * - `variables`: The variables extracted from the input string.
+ * - `keyword`: The keyword extracted from the input string.
+ * - `source`: The original matched source string.
+ * - `isFullMatch`: A boolean indicating whether the input string is a full match.
+ *
+ * @example
+ * ```typescript
+ * const input = "\_\_season_clothes(season=winter)\_\_";
+ * const result = matchDynamicPromptsWildcards(input);
+ * console.log(result);
+ * // Output: { name: 'season_clothes', variables: '(season=winter)', keyword: undefined, source: '\__season_clothes(season=winter)\__', isFullMatch: true }
+ * ```
+ *
+ * @example
+ * __season_clothes(season=winter)__
+ * __season_clothes(season=__season_clothes__)__
+ * __season_clothes(season=!__season_clothes__)__
+ *
+ * __season_clothes(season=__@season_clothes__)__
+ * __season_clothes(season=__~season_clothes__)__
+ *
+ * __@season_clothes(season=__season_clothes__)__
+ * __~season_clothes(season=__season_clothes__)__
+ *
+ * __season_clothes(season={summer|autumn|winter|spring})__
+ * __season_clothes(season=!{summer|autumn|winter|spring})__
+ *
+ * __season_clothes(season={@summer|autumn|winter|spring})__
+ * __season_clothes(season={!summer|autumn|winter|spring})__
+ *
+ * __season_clothes(season=)__
+ */
+export declare function matchDynamicPromptsWildcards(input: string): {
+	name: string;
+	variables: string;
+	keyword: string;
+	source: string;
+	isFullMatch: boolean;
+};
+/**
+ * Checks if the given name is a valid Wildcards name.
+ *
+ * @param name - The name to check.
+ * @returns A boolean indicating whether the name is valid.
+ *
+ * @remarks
+ * A valid Wildcards name should:
+ * - Only contain alphanumeric characters, hyphens, or underscores.
+ * - Not start or end with an underscore.
+ * - Not contain consecutive underscores.
+ *
+ * @example
+ * ```typescript
+ * const name1 = "season_clothes";
+ * console.log(isWildcardsName(name1)); // Output: true
+ *
+ * const name2 = "_season_clothes";
+ * console.log(isWildcardsName(name2)); // Output: false
+ *
+ * const name3 = "season_clothes_";
+ * console.log(isWildcardsName(name3)); // Output: false
+ *
+ * const name4 = "season__clothes";
+ * console.log(isWildcardsName(name4)); // Output: false
+ *
+ * const name5 = "season-clothes";
+ * console.log(isWildcardsName(name5)); // Output: true
+ * ```
+ */
+export declare function isWildcardsName(name: string): boolean;
+export declare function assertWildcardsName(name: string): void;
 export interface IOptionsValidWildcardsYaml {
 	allowMultiRoot?: boolean;
 }
