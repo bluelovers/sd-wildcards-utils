cc1150cb07479c7ba6aa8dc82100e5addf38ce3e
bluelovers <codelovers@users.sourceforge.net>
2024-05-28 01:14:02 +0800

    refactor: add `_validSeq`

--- src/valid.ts
+++ src/valid.ts
@@ -1,14 +1,33 @@
-import { Document, isDocument, isMap, isNode, YAMLMap } from 'yaml';
-import { IWildcardsYAMLDocument, visitWildcardsYAML } from './items';
+import { Document, isDocument, isMap, isNode, isScalar, YAMLMap, YAMLSeq, Scalar } from 'yaml';
+import { IOptionsVisitor, IWildcardsYAMLDocument, visitWildcardsYAML } from './items';
 import { IOptionsSharedWildcardsYaml } from './options';
 import { IRecordWildcards } from './index';
 
-export function _validMap(key: number | 'key' | 'value' | null, node: YAMLMap)
+// @ts-ignore
+export function _validMap(key: number | 'key' | 'value' | null, node: YAMLMap, ...args: any[])
 {
-	const elem = node.items.find(pair => pair.value === null);
+	const elem = node.items.find(pair => pair?.value == null);
 	if (elem)
 	{
-		throw new SyntaxError(`Invalid SYNTAX. key: ${key}, node: ${node}`)
+		throw new SyntaxError(`Invalid SYNTAX. key: ${key}, node: ${node}, elem: ${elem}`)
+	}
+}
+
+// @ts-ignore
+export function _validSeq(key: number | 'key' | 'value' | null, node: YAMLSeq, ...args: any[]): asserts node is YAMLSeq<Scalar | Scalar.Parsed>
+{
+	const index = node.items.findIndex(node => !isScalar(node));
+	if (index !== -1)
+	{
+		throw new SyntaxError(`Invalid SYNTAX. key: ${key}, node: ${node}, index: ${index}, node: ${node.items[index]}`)
+	}
+}
+
+export function createDefaultVisitWildcardsYAMLOptions(): Exclude<IOptionsVisitor, Function>
+{
+	return {
+		Map: _validMap,
+		Seq: _validSeq,
 	}
 }
 
@@ -23,9 +42,7 @@ export function validWildcardsYamlData<T extends IRecordWildcards | IWildcardsYA
 			throw TypeError(`The 'contents' property of the provided YAML document must be a YAMLMap. Received: ${data.contents}`)
 		}
 
-		visitWildcardsYAML(data, {
-			Map: _validMap,
-		});
+		visitWildcardsYAML(data, createDefaultVisitWildcardsYAMLOptions());
 
 		data = data.toJSON()
 	}
