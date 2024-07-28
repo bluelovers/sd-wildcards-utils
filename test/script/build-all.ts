ccc7f043b6ee8ec5dffbfa6333278bd4a8e06299
bluelovers <codelovers@users.sourceforge.net>
2024-07-28 09:03:44 +0800

    .

--- test/script/build-all.ts
+++ test/script/build-all.ts
@@ -1,13 +1,15 @@
-
 async function lazyImport(m: any)
 {
 	m = await m;
 	return m.default ?? m
 }
 
-export default (async () => {
+export default (async () =>
+{
 	await lazyImport(await import('./output/copy'));
 	await lazyImport(await import('./output/split'));
 	await lazyImport(await import('./output/build'));
 	await lazyImport(await import('./check'));
-})();
+})().then(() => console.log(new Date().toLocaleString()));
+
+
