c683be598a9c1232e1750e580c019e0ce12b0122
bluelovers <codelovers@users.sourceforge.net>
2024-10-27 03:29:58 +0800

    refactor!: update format handler

--- src/format.ts
+++ src/format.ts
@@ -4,25 +4,42 @@ export function stripZeroStr(value: string)
 {
 	return value
 		.replace(/[\x00\u200b]+/g, '')
-		.replace(/^[\s\xa0]+|[\s\xa0]+$/gm, '')
 }
 
 export function trimPrompts(value: string)
 {
 	return value
+		.replace(/\xa0/g, ' ')
 		.replace(/^\s+|\s+$/g, '')
+		.replace(/^\s+|\s+$/gm, '')
 		.replace(/\n\s*\n/g, '\n')
+		.replace(/\s+/gm, ' ')
+		.replace(/[ ,.]+(?=,|$)/gm, '')
+		.replace(/,\s*(?=,|$)/g, '')
 		;
+}
+
+export function normalizeWildcardsYamlString(value: string)
+{
+	value = stripZeroStr(value)
+		.replace(/\xa0/g, ' ')
+		.replace(/[,.]+(?=,)/gm, '')
+		.replace(/[ .]+$/gm, '')
+		.replace(/\{\s+(\d+(?:\.\d+)?(?:-(?:\d+(?:\.\d+)?)?\$\$|::))/gm, '{$1')
+		.replace(/\|\s(\d+(?:\.\d+)?::)/gm, '|$1')
+		.replace(/^[ \t]+-[ \t]*$/gm, '')
+		.replace(/^([ \t]+-)[ \t][ ,.]+/gm, '$1 ')
+	;
+	return value
 }
 
 export function formatPrompts(value: string, opts?: IOptionsSharedWildcardsYaml)
 {
 	opts ??= {};
 
-	value = value
-		.replace(/[\s\xa0]+/gm, ' ')
-		.replace(/[\s,.]+(?=,|$)/gm, '')
-	;
+	value = stripZeroStr(value);
+	value = trimPrompts(value);
+	value = normalizeWildcardsYamlString(value);
 
 	if (opts.minifyPrompts)
 	{
@@ -37,10 +54,19 @@ export function formatPrompts(value: string, opts?: IOptionsSharedWildcardsYaml)
 	return value
 }
 
-export function stripBlankLines(value: string)
+export function stripBlankLines(value: string, appendEOF?: boolean)
 {
-	return value
+	value = value
 		.replace(/(\r?\n)[\s\r\n\t\xa0]+(\r?\n)/g, '$1$2')
 		.replace(/(\r?\n)(?:\r?\n)(?=[\s\t\xa0])/g, '$1')
 		.replace(/[ \xa0\t]+$/gm, '')
+	;
+
+	if (appendEOF)
+	{
+		value = value.replace(/\s+$/, '');
+		value += '\n\n';
+	}
+
+	return value
 }
