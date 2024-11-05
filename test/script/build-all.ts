ab4e7613793e488353e454374b65e28afdfc1c95
bluelovers <codelovers@users.sourceforge.net>
2024-11-06 01:57:17 +0800

    .

--- test/script/build-all.ts
+++ test/script/build-all.ts
@@ -1,6 +1,7 @@
 import { copy } from 'fs-extra';
 import { join } from 'path';
 import { __ROOT_OUTPUT_WILDCARDS } from '../__root';
+import { consoleLogger } from 'debug-color2/logger';
 
 async function lazyImport(m: any)
 {
@@ -15,18 +16,21 @@ export default (async () =>
 	await lazyImport(await import('./output/copy'));
 	await lazyImport(await import('./output/split'));
 	await lazyImport(await import('./output/build'));
-	await lazyImport(await import('./check'));
-
-	await lazyImport(await import('./output/build-zip'));
 
 	await copy(join(__ROOT_OUTPUT_WILDCARDS, 'lazy-wildcards.yaml'), join('S:/.data/wildcards_dy', 'lazy-wildcards.yaml'), {
 		preserveTimestamps: true,
 		overwrite: true,
-	}).catch(e => console.error(String(e)))
+	})
+		.then(() => consoleLogger.info('Copied lazy-wildcards.yaml to stable-diffusion-webui'))
+		.catch(e => consoleLogger.error(String(e)))
+	;
+
+	await lazyImport(await import('./check'));
+	await lazyImport(await import('./output/build-zip'));
 
 	let end = new Date();
 
-	console.log(start.toLocaleString(), '->', end.toLocaleString(), 'total', (end.getTime() - start.getTime()) / 1000)
+	consoleLogger.log(start.toLocaleString(), '->', end.toLocaleString(), 'total', (end.getTime() - start.getTime()) / 1000)
 
 })();
 
