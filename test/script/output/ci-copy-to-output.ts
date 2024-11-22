ff4cf697b5168faad5468a43e53c3f25d2f1c9e1
bluelovers <codelovers@users.sourceforge.net>
2024-11-23 03:14:39 +0800

    ci: merge script

--- test/script/output/ci-copy-to-output.ts
+++ test/script/output/ci-copy-to-output.ts
@@ -2,8 +2,13 @@
 
 import { __ROOT, __ROOT_OUTPUT } from '../../__root';
 // @ts-ignore
+import Bluebird from 'bluebird';
+// @ts-ignore
 import { copy } from 'fs-extra';
 import { join } from 'path';
+// @ts-ignore
+import { globSync } from 'fs';
+import { consoleLogger } from 'debug-color2/logger';
 
 export default (async () => {
 
@@ -11,6 +16,20 @@ export default (async () => {
 		preserveTimestamps: true,
 		dereference: true,
 	});
+
+	await Bluebird.each(globSync([
+		'.github/workflows/build.yml',
+		'.github/workflows/valid-yaml.yml',
+		'docs/**/*',
+	], {
+		cwd: __ROOT,
+	}), (file: string) => {
+		consoleLogger.debug(`copy`, file);
+		return copy(join(__ROOT, file), join(__ROOT_OUTPUT, file), {
+			preserveTimestamps: true,
+			dereference: true,
+		})
+	})
 
 })();
 
