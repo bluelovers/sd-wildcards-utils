30b42beadc47115619e5a0192d8a7ae188057de1
bluelovers <codelovers@users.sourceforge.net>
2024-12-30 18:13:16 +0800

    fix(merge): fix error merge when single seq

--- test/merge.spec.ts
+++ test/merge.spec.ts
@@ -4,7 +4,12 @@
 /// <reference types="expect" />
 
 import { basename, extname } from 'path';
-import { parseWildcardsYaml, mergeFindSingleRoots, stringifyWildcardsYamlData } from '../src/index';
+import {
+	parseWildcardsYaml,
+	mergeFindSingleRoots,
+	stringifyWildcardsYamlData,
+	IParseWildcardsYamlInputSource,
+} from '../src/index';
 
 beforeAll(async () =>
 {
@@ -16,20 +21,22 @@ describe(basename(__filename, extname(__filename)), () =>
 
 	test.skip(`dummy`, () => {});
 
-	test(`mergeFindSingleRoots`, () =>
-	{
-		let souce = `
+	const souce = `
 root:
   root2:
     sub1:
+      - 123
+    sub1-1:
       - 123
     sub2:
       sub2-1:
         - 456
       sub2-2:
         - 789
-`
+`;
 
+	test(`mergeFindSingleRoots`, () =>
+	{
 		let souce2 = `
 root:
   root2:
@@ -42,38 +49,70 @@ root:
         - 789
 `
 
-		let doc = parseWildcardsYaml(souce, {
-			allowEmptyDocument: true,
-			keepSourceTokens: false,
-		});
-
-		let doc2 = parseWildcardsYaml(souce2, {
-			allowEmptyDocument: true,
-			keepSourceTokens: false,
-		});
-
-		let actual = mergeFindSingleRoots(doc, doc2);
-
-		expect(actual).toMatchSnapshot();
-
-		expect(stringifyWildcardsYamlData(actual)).toMatchSnapshot();
+		_mergeFindSingleRoots(souce, souce2);
 
 	});
 
-	test(`mergeFindSingleRoots`, () =>
+	test(`mergeFindSingleRoots: Single Seq`, () =>
 	{
-		let souce = `
+		let souce2 = `
 root:
   root2:
     sub1:
       - 123
-    sub2:
+`
+
+		_mergeFindSingleRoots(souce, souce2);
+
+		souce2 = `
+root:
+  root2:
+    sub1:
+      - 123456
+`
+
+		_mergeFindSingleRoots(souce, souce2);
+
+	});
+
+	test(`mergeFindSingleRoots: multi merge`, () =>
+	{
+		let souce2 = `
+root:
+  root2:
+    sub1:
+      - 123456
+    sub1-1:
+      - 123456
+    sub4:
       sub2-1:
         - 456
       sub2-2:
         - 789
 `
 
+		_mergeFindSingleRoots(souce, souce2)
+
+	});
+
+	test(`mergeFindSingleRoots: deep merge: level 2`, () =>
+	{
+		let souce2 = `
+root:
+  root2:
+    sub1:
+      - 123456
+    sub2:
+      sub2-1:
+        - 456789
+`
+
+		expect(() => _mergeFindSingleRoots(souce, souce2)).toThrowErrorMatchingSnapshot();
+
+	});
+
+	test(`mergeFindSingleRoots: throw`, () =>
+	{
 		let souce2 = `
 root:
   root2:
@@ -86,18 +125,34 @@ root:
         - 789
 `
 
-		let doc = parseWildcardsYaml(souce, {
-			allowEmptyDocument: true,
-			keepSourceTokens: false,
-		});
-
-		let doc2 = parseWildcardsYaml(souce2, {
-			allowEmptyDocument: true,
-			keepSourceTokens: false,
-		});
-
-		expect(() => mergeFindSingleRoots(doc, doc2)).toThrowErrorMatchingSnapshot();
+		expect(() => _mergeFindSingleRoots(souce, souce2)).toThrowErrorMatchingSnapshot();
 
 	});
 
 })
+
+function _mergeFindSingleRoots(souce: IParseWildcardsYamlInputSource, souce2: IParseWildcardsYamlInputSource)
+{
+	let doc = parseWildcardsYaml(souce, {
+		allowEmptyDocument: true,
+		keepSourceTokens: false,
+	});
+
+	let doc2 = parseWildcardsYaml(souce2, {
+		allowEmptyDocument: true,
+		keepSourceTokens: false,
+	});
+
+	let actual = mergeFindSingleRoots(doc, doc2);
+	let actualString = stringifyWildcardsYamlData(actual);
+
+	expect(actual).toMatchSnapshot();
+	expect(actualString).toMatchSnapshot();
+
+	return {
+		doc,
+		doc2,
+		actual,
+		actualString,
+	} as const
+}
