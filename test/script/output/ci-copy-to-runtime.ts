63e12b795070fa239373b634f9eddffd4073b7b9
bluelovers <codelovers@users.sourceforge.net>
2025-07-24 13:00:21 +0800

    ci: update script

--- test/script/output/ci-copy-to-runtime.ts
+++ test/script/output/ci-copy-to-runtime.ts
@@ -5,7 +5,7 @@ import { globSync } from 'fs';
 import Bluebird from 'bluebird';
 // @ts-ignore
 import { copy, exists } from 'fs-extra';
-import { join } from 'path';
+import { join, extname } from 'upath2';
 import { consoleLogger } from 'debug-color2/logger';
 
 export default (async () => {
@@ -13,30 +13,46 @@ export default (async () => {
 	const __SRC_DIR = join(__ROOT_DATA, 'others');
 	const __OUT_DIR = join('S:/.data/wildcards_dy');
 
-	await exists(__OUT_DIR)
-		.then(() => {
-			return Bluebird.each(globSync([
-				'Vision/**/*.yaml',
-				'user-*/**/*.yaml',
-			], {
-				cwd: __SRC_DIR,
-			}), async (file: string) => {
+	const bool = await exists(__OUT_DIR);
 
-				if (!await exists(join(__OUT_DIR, file))) {
+	consoleLogger[bool ? 'yellow' : 'red'].debug(`detect`, bool, __OUT_DIR);
+
+	if (bool)
+	{
+		await Bluebird.each(globSync([
+			'Vision/**/*.{yaml,txt}',
+			'user-*/**/*.{yaml,txt}',
+			'!**/_disable/*',
+			'!**/_*.{yaml,txt}',
+		], {
+			cwd: __SRC_DIR,
+		}), async (file: string) => {
+
+			const src_file = join(__OUT_DIR, file);
+
+			if (!await exists(src_file))
+			{
+				let skip = (extname(file) === '.txt' && await exists(src_file + '.yaml')) || await exists(src_file + '.disable');
+
+				if (skip)
+				{
+					consoleLogger.gray.debug(`skip`, file);
+				}
+				else
+				{
 					consoleLogger.yellow.debug(`copy`, file);
-					return copy(join(__SRC_DIR, file), join(__OUT_DIR, file), {
+					return copy(src_file, join(__OUT_DIR, file), {
 						preserveTimestamps: true,
 						dereference: true,
 					})
 				}
+			}
 
-			});
 		})
-		.catch(e => {
-			consoleLogger.error(String(e), e)
-			consoleLogger.dir(e)
-		})
-	;
+	}
 
-})();
+})().catch(e => {
+	consoleLogger.error(String(e), e)
+	consoleLogger.dir(e)
+});
 
