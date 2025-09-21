d7e96a91514334839db111daa5f764f377ca8865
bluelovers <codelovers@users.sourceforge.net>
2025-09-21 09:34:06 +0800

    feat!: 允許使用 '

--- src/const.ts
+++ src/const.ts
@@ -1,4 +1,5 @@
 export const RE_UNSAFE_QUOTE = /['"]/;
+export const RE_UNSAFE_QUOTE_DOUBLE = /"/;
 export const RE_UNSAFE_VALUE = /^\s*-|[{$~!@}\n|:?#'"%]/;
 //export const RE_UNSAFE_PLAIN = /-/;
 export const RE_UNSAFE_PLAIN = /-/;
